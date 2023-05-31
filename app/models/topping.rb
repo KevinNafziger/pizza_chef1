680d7f2beea744715837922c9286316699f758ac
EC2 Default User <ec2-user@ip-172-31-90-226.ec2.internal>
2023-05-31 01:32:31 +0000

    fix so new and edit views do not have nav_bars at top

--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,5 +1,5 @@
 class Topping < ApplicationRecord
-  has_many :pizza_toppings
+  has_many :pizza_toppings, inverse_of: :topping, dependent: :destroy
   has_many :pizzas, through: :pizza_toppings
   validates :name, uniqueness: true, presence: true
 end
