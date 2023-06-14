4146e7345c528ab703be8e764f3db57bbe30cfa6
EC2 Default User <ec2-user@ip-172-31-90-226.ec2.internal>
2023-06-14 04:47:47 +0000

    update tests

--- spec/models/topping_spec.rb
+++ spec/models/topping_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Topping, type: :model do
   let(:topping) { build(:topping) }
-
   it { should validate_uniqueness_of(:name) }
   it { should validate_presence_of(:name) }
 end
